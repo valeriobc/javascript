639f3e0cd5720351a0320ad828aa371c50bb98d9
valeriobc <57022536+valeriobc@users.noreply.github.com>
2020-11-08 14:57:25 -0300

    update ex014

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,14 @@
-alert('Olá!')
+function carregar() {
+   var msg = window.document.getElementById('msg')
+   var img = window.document.getElementById('imagem')
+   var data = new Date()
+   //var hora = data.getHours()
+   var hora = 8
+   msg.innerHTML = `Agora são ${hora} horas`
+   if (hora >= 0 && hora < 12) {
+      img.src = 'fotomanha.png'
+   } else if (hora >= 12 && hora < 18) {
+      img.src = 'fototarde.png'
+   } else {
+      img.src = 'fotonoite.png'
+   }
