2e2c321171c24b1fed7a116fb6b328c89eba4ec6
valeriobc <57022536+valeriobc@users.noreply.github.com>
2020-11-08 15:39:25 -0300

    update ex014

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -3,12 +3,16 @@ function carregar() {
    var img = window.document.getElementById('imagem')
    var data = new Date()
    //var hora = data.getHours()
-   var hora = 8
+   var hora = 20
    msg.innerHTML = `Agora são ${hora} horas`
    if (hora >= 0 && hora < 12) {
       img.src = 'fotomanha.png'
+      window.document.body.style.background = '#e2cd9f'
    } else if (hora >= 12 && hora < 18) {
       img.src = 'fototarde.png'
+      window.document.body.style.background = '#b9846f'
    } else {
       img.src = 'fotonoite.png'
+      window.document.body.style.background = '#515154'
    }
+}
