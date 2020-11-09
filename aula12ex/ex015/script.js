678c9c45b30a5f6d0e250cb4ebc9e4a9cbabd4a5
valeriobc <57022536+valeriobc@users.noreply.github.com>
2020-11-08 21:53:01 -0300

    create/update ex015

---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,58 @@
+function verificar() {
+   var data = new Date()
+   var ano = data.getFullYear()
+   //var fano = window.document.getElementById('txtano')
+   var fano = window.document.querySelector('input#txtano')
+   var res = window.document.querySelector('div#res')
+   if (fano.value.length == 0 || Number(fano.value) > ano) {
+      window.alert('[ERRO] Verifique os dados e tente novamente!')
+   } else {
+      var fsex = window.document.getElementsByName('radsex')
+      var idade = ano - Number(fano.value)
+      var genero = ''
+      var img = window.document.createElement('img')
+      img.setAttribute('id', 'foto')
+      if (fsex[0].checked) {
+         genero = 'Homem'
+         if (idade >=0 && idade < 10) {
+            //Criança
+            img.setAttribute('src', 'foto-bebe-m.png')
+            img.setAttribute('alt', 'Foto Bebê Masculino')
+         } else if (idade < 21) {
+            //Jovem
+            img.setAttribute('src', 'foto-jovem-m.png')
+            img.setAttribute('alt', 'Foto Jovem Masculino')
+         } else if (idade < 50) {
+            //Adulto
+            img.setAttribute('src', 'foto-adulto-m.png')
+            img.setAttribute('alt', 'Foto Adulto Masculino')
+         } else {
+            //Idoso
+            img.setAttribute('src', 'foto-idoso-m.png')
+            img.setAttribute('alt', 'Foto Idoso Masculino')
+         }
+      } else if (fsex[1].checked) {
+         genero = 'Mulher'
+         if (idade >=0 && idade < 10) {
+            //Criança
+            img.setAttribute('src','foto-bebe-f.png')
+            img.setAttribute('alt', 'Foto Bebê Menina')
+         } else if (idade < 21) {
+            //Jovem
+            img.setAttribute('src', 'foto-jovem-f.png')
+            img.setAttribute('alt', 'Foto Mulher Jovem')
+         } else if (idade < 50) {
+            //Adulto
+            img.setAttribute('src', 'foto-adulto-f.png')
+            img.setAttribute('alt', 'Foto Mulher Adulta')
+         } else {
+            //Idoso
+            img.setAttribute('src', 'foto-idoso-f.png')
+            img.setAttribute('alt', 'Foto Mulher Idosa')
+         }
+      }
+      res.style.textAlign = 'center'
+      res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+      res.appendChild(img) 
+   }
+}
