24212c77b71b6090035342b9ca88a63f4295c1e6
valeriobc <57022536+valeriobc@users.noreply.github.com>
2020-11-08 22:46:11 -0300

    Create ambiente.js

---
+++ aula13/ambiente.js
@@ -0,0 +1,14 @@
+var c = 1
+while (c < 6) {
+   console.log('Tudo Bem?')
+   c++
+}
+
+
+/*console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')*/
