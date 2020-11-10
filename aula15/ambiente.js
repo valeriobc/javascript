ca49aff2d72f6aa9adee30c6a97f8fde76b6b690
valeriobc <57022536+valeriobc@users.noreply.github.com>
2020-11-09 21:31:43 -0300

    create ex015

---
+++ aula15/ambiente.js
@@ -0,0 +1,9 @@
+let num = [5, 8, 4]
+num[3] = 6
+num.push(7)
+console.log(num)
+num.sort()
+console.log(num)
+console.log(`O vetor tem ${num.length} posiçoes`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+
---
+++ aula15/buscaemvetor.js
@@ -0,0 +1,7 @@
+let num = [5, 8, 2, 9, 3]
+let pos = num.indexOf(8)
+if (pos == -1) {
+   console.log('O valor não foi encontrado!')
+} else {
+   console.log(`O valor está na posição ${pos}`)
+}
