41fa0832687d192cb82fde819bf8fa4b05e8de94
valeriobc <57022536+valeriobc@users.noreply.github.com>
2020-11-11 11:16:29 -0300

    update

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -7,13 +7,13 @@ numobj.focus()
 function adicionar() {
    
    let selobj = window.document.querySelector('select#selana')
-   let numexiste = num.indexOf(numobj.value)
-   if (numobj.value.length == 00 || numobj.value == 00 || numobj.value > 99 || numexiste != -1) {
+   let numexiste = num.indexOf(Number(numobj.value))
+   if (numobj.value.length == 00 || numobj.value == 00 || numobj.value > 100 || numexiste != -1) {
       window.alert('[ERRO!] Valor inválido ou já encontrado na lista!')
    } else {
       num.push(Number(numobj.value))
       let item = window.document.createElement('option')
-      item.text = `Valor ${num[pos]} adiccionado na p${numexiste}}`
+      item.text = `Valor ${numobj.value} adiccionado na p${num.length-1}`
       selobj.appendChild(item)
       resobj.innerHTML = ''
    }
@@ -25,17 +25,22 @@ function finalizar() {
    if (num.length == 0) {
       window.alert('Adicione valores antes de finalizar')
    } else {
-      
       resobj.innerHTML = ''
       resobj.innerHTML += `<p>Ao todo, temos ${num.length} números cadastrados.</p>`
-      num.sort()
-      let ultnum = num[num.length - 1]
-      resobj.innerHTML += `<p>O maior valor informado foi ${ultnum}.</p>`
-      resobj.innerHTML += `<p>O menor valor informado foi ${num[0]}.</p>`
-      var soma = 0
-      for (let c in num) {
-         soma += Number(num[c])
+      let soma = 0
+      let maior = num[0]
+      let menor = num[0]
+      for(let pos in num) {
+         if(num[pos] > maior) {
+            maior = num[pos]
+         }
+         if(num[pos] < menor) {
+            menor = num[pos]
+         }
+         soma += num[pos]
       }
+      resobj.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+      resobj.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
       resobj.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
       resobj.innerHTML += `<p>A média dos valores digitados é ${(soma / num.length).toFixed(2).replace('.', ',')}.</p>`
    }
