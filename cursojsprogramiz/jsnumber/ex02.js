c37bd4263c7aaf05d3cc997e2c39b12ff06370de
valeriobc <57022536+valeriobc@users.noreply.github.com>
2020-11-19 14:22:23 -0300

    update

--- cursojsprogramiz/jsnumber/ex02.js
+++ cursojsprogramiz/jsnumber/ex02.js
@@ -1,2 +1,6 @@
-const a = - 2 / 0;
-console.log(a);
+// valor BigInt
+const value = 9007199925124740998n;
+
+// somando 2 BigInt
+const value1 = value + 1n;
+console.log(value1); // retorna "9007199925124740999n"
---
+++ cursojsprogramiz/jsnumber/ex03.js
@@ -0,0 +1,12 @@
+const a = 45;
+
+// criando um objeto número
+const b = new Number(45);
+
+console.log(a); // 45
+
+console.log(b); // [Number: 45]
+
+console.log(typeof a); // number
+
+console.log(typeof b); // object
