87fde07dd255bbc309f0a80eb734a79c29756457
valeriobc <57022536+valeriobc@users.noreply.github.com>
2020-11-25 07:47:36 -0300

    Create ex02.js

---
+++ cursojsprogramiz/jsdatetime/ex02.js
@@ -0,0 +1,8 @@
+const time1 = new Date(0);
+
+// epoch time
+console.log(time1); // 1970-01-01T00:00:00.000Z
+
+// 100000000000 milisegundos após epoch time
+const time2 = new Date(100000000000);
+console.log(time2); // 1973-03-03T09:46:40.000Z
